2841049fe475eba306e635093cfa02e3258be8a6
Ishan Pathak <ishan.pathak2711@gmail.com>
2025-07-01 15:08:51 -0400

    removed features grid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import HeroBanner from '../components/HeroBanner';
-import FeaturesGrid from '../components/FeaturesGrid';
+
 import TopBooksGrid from '../components/TopBooksGrid';
 import FeaturedBooks from '../components/FeaturedBooks';
 import FeaturedAuthor from '../components/FeaturedAuthor';
@@ -15,9 +15,8 @@ const Home = () => {
     <div className="font-sans bg-gray-100 min-h-screen">
       <Navbar />
       <HeroBanner />
-      <FeaturesGrid />
-      <FeaturedBooks />
       <FeaturedAuthor />
+      <FeaturedBooks />
       <FavouriteReads />
       <ShopByCategory />
       <TopBooksGrid />
